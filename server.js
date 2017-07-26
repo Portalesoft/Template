7680463df64d3156a3a877b7c6c2824a833ddc13
Barry Jacobs <portalesoft@outlook.com>
2017-07-26 15:11:55 +0100

    Modified production status

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const app = express();
 app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
 // Default route processing
-if (process.env.NODE_ENV !== 'production') {
-
-    // Development resources not required in production
-    const webpackMiddleware = require('webpack-dev-middleware');
-    const webpack = require('webpack');
-    const webpackConfig = require('./webpack.config.js');
-    app.use(webpackMiddleware(webpack(webpackConfig)));
-
-} else {
+if (process.env.NODE_ENV === 'production') {
 
     // GZIP all assets
     app.use(compression());
@@ -28,6 +20,14 @@ if (process.env.NODE_ENV !== 'production') {
     app.get('*', (req,res) => {
         res.sendFile(path.join(__dirname, 'dist/index.html'));
     });
+
+} else {
+
+    // Development resources not required in production
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config.js');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
       template: 'src/templates/index.html'
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
